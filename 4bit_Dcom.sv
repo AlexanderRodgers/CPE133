6797fe429b17897c0870cc34af0fbc85b6c7275a
Alexander Rodgers <alexedrodgers@gmail.com>
2019-05-13 01:02:00 -0700

    added starting files

---
+++ 4bit_Dcom.sv
@@ -0,0 +1,30 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 05/10/2019 11:06:00 AM
+// Design Name: 
+// Module Name: 4bit_Dcom
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module 4bit_Dcom(
+    input [3:0] D,
+    input clk,
+    input clr,
+    input set,
+    output [3:0] Q
+    );
+endmodule
